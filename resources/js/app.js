b968230251e0c39873c34165ee4459fd64534828
Isabel Lovera <isabel5200.v@gmail.com>
2025-03-16 16:51:06 -0700

    Installing and testing PrimeVue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,9 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import Button from "primevue/button"
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -19,6 +22,12 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PrimeVue, {
+                theme: {
+                    preset: Aura
+                }
+            })
+            .component('Button', Button)
             .mount(el);
     },
     progress: {
