e49352b7c6d448a71b1dac706af90277f9302722
Isabel Lovera <isabel5200.v@gmail.com>
2025-03-16 23:19:37 -0700

    Creating policies

---
+++ app/Policies/Budget.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Budget;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class Budget
+{
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    public function view(User $user, Budget $budget): bool
+    {
+        return $user->id === $budget->user_id;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Budget $budget): bool
+    {
+        return $user->id === $budget->user_id;
+    }
+
+    public function delete(User $user, Budget $budget): bool
+    {
+        return $user->id === $budget->user_id;
+    }
+
+    public function restore(User $user, Budget $budget): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, Budget $budget): bool
+    {
+        return false;
+    }
+}
---
+++ app/Policies/Transaction.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Transaction;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class Transaction
+{
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    public function view(User $user, Transaction $transaction): bool
+    {
+        return $user->id === $transaction->user_id;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Transaction $transaction): bool
+    {
+        return $user->id === $transaction->user_id;
+    }
+
+    public function delete(User $user, Transaction $transaction): bool
+    {
+        return $user->id === $transaction->user_id;
+    }
+
+    public function restore(User $user, Transaction $transaction): bool
+    {
+        return false;
+    }
+
+    public function forceDelete(User $user, Transaction $transaction): bool
+    {
+        return false;
+    }
+}
